ef9c3ba47fc87cb2fbe817870bd8f3dc04adcd0e
Saulo Joab <saulojoabps@gmail.com>
2022-06-28 10:23:30 -0300

    feat: adding project structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Router } from "@reach/router";
+import { Admin, Display, Terminal } from "pages";
+
+export default function App() {
+  return (
+    <Router>
+      <Terminal path="/" />
+      <Admin path="/admin" />
+      <Display path="/display" />
+    </Router>
+  );
+}
---
+++ src/pages/Terminal/Terminal.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Terminal() {
+  return (
+    <div>
+      <h1>Terminal</h1>
+    </div>
+  );
+}
